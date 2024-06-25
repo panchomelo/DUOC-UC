f7d276127caa2ebcdb9e4e469e31ab79c6288643
panchoMelo <fran.vgasalv@gmail.com>
2024-06-25 01:02:51 -0400

    sjadlaksj

--- funciones.py
+++ funciones.py
@@ -1,1 +1,34 @@
 def ingresar_paciente():
+    pacientes = []
+
+    while True:
+        print("Ingrese los datos del nuevo paciente")
+        nombre_completo=input("Ingrese el nombre completo del paciente")
+        rut=input("Ingrese el rut del paciente")
+        sexo=input("Ingrese el sexo del paciente")
+        diagnostico=input("Ingrese el diagnostico del paciente")
+        print("Ingrese los medicamentos recetados al paciente. Tipea 'FIN' para terminar")
+
+        medicamentos = []
+        while True:
+            medicamento=input("-Medicamento: ")
+            if medicamento.lower() == "fin":
+                break
+            medicamentos.append(medicamento)
+
+        paciente = {
+            'nombre_completo': nombre_completo,
+            'rut': rut,
+            'sexo': sexo,
+            'diagnostico': diagnostico,
+            'medicamentos_recetados': medicamentos
+        }
+
+        pacientes.append(paciente)
+
+        continuar=input("¿Desea ingresar otro paciente? (S/N): ")
+        if continuar.lower() != 's':
+            break
+
+    return pacientes
+
