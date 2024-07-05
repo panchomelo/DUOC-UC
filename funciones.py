dff8726737e7acb4893d548da9d2a4dc74a368a5
panchoMelo <fran.vgasalv@gmail.com>
2024-07-05 18:26:28 -0400

    askkdjlas

--- funciones.py
+++ funciones.py
@@ -1,49 +1,124 @@
 import json
+import numpy as np
+global matriz
 
 def menu():
+    print("¿Qué opción desea realizar?")
     print("1.- Ingresar ficha del paciente")
-    print("2.- Buscar ficha por rut")
-    print("3.- Buscar medicamentos por rut")
+    print("2.- Buscar ficha del paciente")
+    print("3.- Buscar medicamentos recetados")
     print("4.- Eliminar ficha del paciente")
-    print("5.- Listar pacientes atendidos")
-    print("6.- Salir")
+    print("5.- Listar pacientes ingresados")
+    print("6.- Para salir y actualizar pacientes")
     op = int(input("Ingrese una opción válida (1/6): "))
     return op
 
 
-def ingresar_paciente():
-    pacientes = []
+def es_numero(valor):
+    return isinstance(valor, (int, float))
 
+
+def ingresar_paciente(matriz):
     while True:
-        print("Ingrese los datos del nuevo paciente:")
-        nombre_completo=input("Ingrese el nombre completo del paciente ")
-        rut=input("Ingrese el rut del paciente ")
-        sexo=input("Ingrese el sexo del paciente ")
-        diagnostico=input("Ingrese el diagnostico del paciente ")
-        print("Ingrese los medicamentos recetados al paciente. Tipea 'FIN' para terminar")
+        fila_vacia = None
 
-        medicamentos = []
-        while True:
-            medicamento=input("-Medicamento: ")
-            if medicamento.lower() == "fin":
+        for i in range(matriz.shape[0]):
+            if matriz[i][0] is None or matriz[i][0] == "":
+                fila_vacia = i
                 break
-            medicamentos.append(medicamento)
 
-        paciente = {
-            'nombre_completo': nombre_completo,
-            'rut': rut,
-            'sexo': sexo,
-            'diagnostico': diagnostico,
-            'medicamentos_recetados': medicamentos
-        }
+        if fila_vacia is None:
+            print("No hay filas vacías para ingresar nuevos pacientes.")
+            return matriz  # Retorna matriz sin cambios si no hay filas vacías
+        
+        print()
+        nombre_completo = input("Ingrese el nombre completo del paciente: ")
+        rut = input("Ingrese el rut del paciente: ")
+        sexo = input("Ingrese el sexo del paciente: ")
+        edad = input("Ingrese la edad del paciente: ")
+        fono = input("Ingrese el fono del paciente: ")
+        diagnostico = input("Ingrese el diagnóstico del paciente: ")
+        medicamento = input("Ingrese el medicamento recetado: ")
 
-        pacientes.append(paciente)
+        # Ingresar los datos del paciente en la fila vacía encontrada
+        matriz[fila_vacia] = [nombre_completo, rut, sexo, edad, fono, diagnostico, medicamento]
+        print()
+        print("Paciente ingresado con éxito en la fila", fila_vacia)
 
-        continuar=input("¿Desea ingresar otro paciente? (S/N): ")
-        with open('datos.json', 'w') as archivo:
-            json.dump(pacientes, archivo, indent=4)
-        if continuar.lower() != 's':
+        respuesta = input("¿Desea ingresar otro paciente? (si/no): ")
+        print()
+        if respuesta.lower() != 'si':
             break
 
-    return pacientes
+    return matriz  # Retorna la matriz global modificada
+
+
+def buscar_paciente(rut, matriz):
+    encontrado=False
+    for paciente in matriz:
+        if paciente[1] == rut:
+            encontrado=True
+            print()
+            print("Datos del paciente:")
+            print(f"Nombre completo: {paciente[0]}")
+            print(f"RUT: {paciente[1]}")
+            print(f"Sexo: {paciente[2]}")
+            print(f"Edad: {paciente[3]}")
+            print(f"Fono: {paciente[4]}")
+            print(f"Diagnóstico: {paciente[5]}")
+            print(f"Medicamento: {paciente[6]}")
+            print()
+
+    if not encontrado:
+        print(f"No se encontró ningún paciente con el RUT {rut}")
+        print()
+
+
+def buscar_medicamento(rut, matriz):
+    encontrado=False
+    for paciente in matriz:
+        if paciente[1] == rut:
+            encontrado=True
+            print()
+            print(f"El medicamento recetado para el paciente {paciente[0]} es {paciente[6]}")
+            print()
+
+    if not encontrado:
+        print(f"No se encontró ningún paciente con el RUT {rut}")
+        print()
+
+
+def eliminar_paciente(rut, matriz):
+    encontrado = False
+    nombre_paciente = None
+
+    for i in range(matriz.shape[0]):
+        if matriz[i, 1] == rut:
+            encontrado = True
+            nombre_paciente = matriz[i, 0]  # matriz[i, 0] es el campo del nombre en la matriz
+            print(f"Eliminando paciente {nombre_paciente} con rut {rut}")
+            matriz = np.delete(matriz, i, axis=0)
+            print("Paciente eliminado correctamente.")
+            print()
+            break
+
+    if not encontrado:
+        print(f"No se encontró ningún paciente con el RUT {rut}")
+        print()
+    else:
+        return matriz
+    
+
+def listar_pacientes(matriz):
+    print()
+    print("Listado de pacientes:")
+    print()
+    for paciente in matriz:
+        if paciente[0] is not None and paciente[0] != "":
+            print(f"{paciente[0]}, {paciente[1]}, {paciente[2]}, {paciente[3]}, {paciente[4]}, {paciente[5]}, {paciente[6]}")
+            print()
+        else:
+            break
+
+
 
--- hospi.py
+++ hospi.py
@@ -1,23 +1,71 @@
-from funciones import menu, ingresar_paciente
+from funciones import menu, ingresar_paciente, buscar_paciente, buscar_medicamento, eliminar_paciente, listar_pacientes
+import json, os
+import numpy as np
+
+matriz = np.empty((50, 7), dtype=object)
+
+if os.path.exists('usuarios.json'):
+    with open('usuarios.json', 'r') as file:
+        json_usuarios = json.load(file)
+
+    for i, usuario in enumerate(json_usuarios):
+        matriz[i, 0] = usuario['nombre_completo']
+        matriz[i, 1] = usuario['rut']
+        matriz[i, 2] = usuario['sexo']
+        matriz[i, 3] = usuario['edad']
+        matriz[i, 4] = usuario['fono']
+        matriz[i, 5] = usuario['diagnostico']
+        matriz[i, 6] = usuario['medicamento']
 
 while True:
     op = menu()
 
     match op:
         case 1:
-            lista_pacientes=ingresar_paciente()
+            matriz=ingresar_paciente(matriz) 
+            #captura y actualiza la matriz global
         case 2:
-            print("Buscar ficha")
+            print()
+            rut_input=input("Ingrese el rut del paciente que desea buscar ")
+            buscar_paciente(rut_input, matriz)
         case 3:
-            print("Buscar medicamentos")
+            print()
+            rut_input=input("Ingresa el rut del paciente ")
+            buscar_medicamento(rut_input, matriz)
         case 4:
-            print("Eliminar ficha")
+            print()
+            rut_input=input("Ingresa el rut del paciente que deseas eliminar ")
+            matriz=eliminar_paciente(rut_input, matriz)
         case 5:
-            print("Listar pacientes")
+            listar_pacientes(matriz)
         case 6:
             print("Has elegido salir. Adiós!")
             break
         case _:
             print("Opción no valida")
-            
+
+diccionario_pacientes = []
+
+for paciente in matriz:
+    if None in paciente:
+        continue
+    
+    paciente_dict = {
+    "nombre_completo": paciente[0],
+    "rut": paciente[1],
+    "sexo": paciente[2],
+    "edad": paciente[3],
+    "fono": paciente[4],
+    "diagnostico": paciente[5],
+    "medicamento": paciente[6]
+    }
+    diccionario_pacientes.append(paciente_dict)                                
+
+with open('usuarios.json', 'w') as file:
+    json.dump(diccionario_pacientes, file, indent=4)
+
+print("Datos de pacientes guardados en usuarios.json exitosamente.")
+
+
+
     
